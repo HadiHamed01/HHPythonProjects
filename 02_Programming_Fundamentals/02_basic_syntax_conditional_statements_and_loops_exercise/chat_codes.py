6deea700565e8033fef92728beef360869a900ce
HadiHamed01 <hadi.hamed@icloud.com>
2025-10-04 11:02:51 +0300

    Committing project

---
+++ 02_Programming_Fundamentals/02_basic_syntax_conditional_statements_and_loops_exercise/chat_codes.py
@@ -0,0 +1,13 @@
+count = int(input())
+
+for i in range(count):
+    num = int(input())
+
+    if num == 88:
+        print("Hello")
+    elif num == 86:
+        print("How are you?")
+    elif num < 88:
+        print("GREAT!")
+    else:
+        print("Bye.")
