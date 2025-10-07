c171a8ad701db450e6be85b2609b78efe9289d25
HadiHamed01 <hadi.hamed@icloud.com>
2025-10-07 19:18:11 +0300

    Committing project

---
+++ 02_Programming_Fundamentals/03_data_types_and_variables/special_numbers.py
@@ -0,0 +1,13 @@
+number = int(input())
+
+for num in range(1, number + 1):
+    num_as_str = str(num)
+    digits_sum = 0
+    for digit in num_as_str:
+        digits_sum += int(digit)
+    is_special = False
+
+    if digits_sum == 5 or digits_sum == 7 or digits_sum == 11:
+        is_special = True
+
+    print(f"{num} -> {is_special}")
