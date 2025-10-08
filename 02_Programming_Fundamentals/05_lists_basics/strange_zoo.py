a5bed71e769d17ea65aced5079fa588dc262abd9
HadiHamed01 <hadi.hamed@icloud.com>
2025-10-09 00:47:50 +0300

    Committing project

---
+++ 02_Programming_Fundamentals/05_lists_basics/strange_zoo.py
@@ -0,0 +1,11 @@
+tail = input()
+body = input()
+head = input()
+
+my_list = [head, body, tail]
+print(my_list)
+
+# OR
+# my_list = [tail, body, head]
+# my_list[0], my_list[2] = my_list[2], my_list[0]
+# this is called swapping
